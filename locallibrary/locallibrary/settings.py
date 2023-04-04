b202965ba4e3f6daf358a01046114d54bef0cf14
jakinpilla <jakinpilla@gmail.com>
2023-04-04 10:28:17 +0900

    Merge remote-tracking branch 'origin/main'

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9w11lsme*swcm74&g+zukd@@+1&k#_)2bsaas@7=_nxouffn3^'
+# SECRET_KEY = 'django-insecure-9w11lsme*swcm74&g+zukd@@+1&k#_)2bsaas@7=_nxouffn3^'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            'django-insecure-9w11lsme*swcm74&g+zukd@@+1&k#_)2bsaas@7=_nxouffn3^')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
